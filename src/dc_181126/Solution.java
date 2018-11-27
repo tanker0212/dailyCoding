448ee82ab0b0d890c04d1b647420d969b89ddd80
lim <tanker0212@gmail.com>
2018-11-27 13:52:05 +0900

    181126_solve

--- src/dc_181126/Solution.java
+++ src/dc_181126/Solution.java
@@ -1,28 +1,44 @@
 package dc_181126;
 
-import java.util.Arrays;
+import java.util.HashSet;
 
-public class Solution {
-    public static void main(String[] args) {
-        int n = 4;
-        //int[][] costs = new int[][]{{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
-        int[][] costs = new int[][]{{0,1,1},{0,2,12},{1,3,10},{2,3,1}};
-        int[] done = new  int[n];
-        // 1. 가장 싼거 하나 연결하고
-        // 2. 그 연결된곳에서 다른곳으로 가는 가장 싼거 하나 연결하고
-        // 3. 다 연결될때까지 2를 반복
-        for (int i = 0; i < n; i++) {
-            int min_cost = Integer.MAX_VALUE;
-            int[] tmp = new int[3];
-            for (int[] cost : costs) {
-                if (cost[0] == i || cost[1] == i){
-                    if (cost[2] < min_cost){
-                        min_cost = cost[2];
-                        tmp = cost;
-                    }
-                }
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+
+        int costs_len = costs.length;
+        int[] check = new int[costs_len];
+        HashSet<Integer> Island_set = new HashSet<>();
+
+        int idx = 0;
+        int min_cost = Integer.MAX_VALUE;
+        for (int i = 0; i < costs_len; i++)
+            if (costs[i][2] < min_cost){
+                min_cost = costs[i][2];
+                idx = i;
             }
-            System.out.println(Arrays.toString(tmp));
+        answer += build(check, Island_set, costs[idx], idx);
+
+        while(Island_set.size() < n) {
+            min_cost = Integer.MAX_VALUE;
+            for (int i = 0; i < costs_len; i++)
+                if (check[i] == 0)
+                    if (Island_set.contains(costs[i][0]) || Island_set.contains(costs[i][1]))
+                        if (!Island_set.contains(costs[i][0]) || !Island_set.contains(costs[i][1]))
+                            if (costs[i][2] < min_cost){
+                                min_cost = costs[i][2];
+                                idx = i;
+                            }
+
+            answer += build(check, Island_set, costs[idx], idx);
         }
+        return answer;
+    }
+
+    private int build(int[] check, HashSet<Integer> Island_set, int[] cost, int idx){
+        check[idx] = 1;
+        Island_set.add(cost[0]);
+        Island_set.add(cost[1]);
+        return cost[2];
     }
 }
